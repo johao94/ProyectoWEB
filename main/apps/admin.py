d910a86bd5a4270ed6f83db9de35cb92690497e8
johao94 <johao.villarroel@udla.edu.ec>
2021-06-23 18:59:08 -0500

    Initial commit

---
+++ main/apps/admin.py
@@ -0,0 +1,69 @@
+import json
+from django.contrib import admin
+
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Count
+from django.db.models.aggregates import Sum
+from django.db.models.functions import TruncDay
+from django.http import JsonResponse
+from django.urls import path
+
+from .models import *
+
+class ProductoA(admin.ModelAdmin):
+    
+    list_display=('nombre', 'precio','provedor', 'fecha')
+    search_fields = ("nombre", )
+
+    def changelist_view(self, request, extra_context=None):
+        chart_data = self.chart_data()
+        as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+        extra_context = extra_context or {"chart_data": as_json}
+        return super().changelist_view(request, extra_context=extra_context)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        extra_urls = [
+            path("chart_data/", self.admin_site.admin_view(self.chart_data_endpoint))
+        ]
+        # NOTE! Our custom urls have to go before the default urls, because they
+        # default ones match anything.
+        return extra_urls + urls
+
+    # JSON endpoint for generating chart data that is used for dynamic loading
+    # via JS.
+    def chart_data_endpoint(self, request):
+        chart_data = self.chart_data()
+        return JsonResponse(list(chart_data), safe=False)
+
+    def chart_data(self):
+        return (
+            Producto.objects.annotate(date=TruncDay("fecha"))
+            .values("date")
+            .annotate(y=Sum("precio"))
+            .order_by("-date")
+        )
+   # def chart_data(self):
+    #    return (
+     #       Producto.objects.annotate(date=TruncDay("fecha"))
+      #      .values("date")
+       #     .annotate(y=Sum("precio"))
+        #    .order_by("-date")
+        #)
+
+class ProvedorA(admin.ModelAdmin):
+    list_display=('nombre',)
+class ClienteA(admin.ModelAdmin):
+    list_display=('nombre', 'apellidos')
+class CompraA(admin.ModelAdmin):
+    list_display=('cliente', 'producto')
+
+
+
+
+admin.site.register(Cliente,ClienteA)
+admin.site.register(Provedor,ProvedorA)
+admin.site.register(Producto,ProductoA)
+admin.site.register(Compra,CompraA)
+
+admin.site.site_header="Administracion"
